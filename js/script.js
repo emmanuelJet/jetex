d66d57895a568f59f9985ebb2c52901901e40c5f
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2018-06-27 01:39:03 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*var objPeople = [
+    {
+        email: "emmanuelj774@gmil.com",
+        password: "temitayo12"
+    },
+    {
+        email: "someone@example.com",
+        password: "matt"
+    },
+    {
+        email: "me@jet.com",
+        password: "jet"
+    }
+]*/
